4e496d3720167c097700ade8b6010b2e25ab90e6
necocoa <43537209+necocoa@users.noreply.github.com>
2021-02-27 14:18:31 +0900

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import { Layout } from "src/components/layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h2>Home</h2>
+      <button
+        onClick={() => {
+          window.alert("Hello, World!");
+        }}
+      >
+        Button
+      </button>
+    </Layout>
+  );
+};
+
+export default Home;
