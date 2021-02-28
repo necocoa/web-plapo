24585fb9212de7eb0788885f2ff742d1eb86e581
necocoa <joyds217@gmail.com>
2021-03-01 02:16:02 +0900

    websocketで送って受け取るまで確認

--- client/src/pages/card.tsx
+++ client/src/pages/card.tsx
@@ -1,16 +1,46 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
 
+type CardType = {
+  userId: string
+  cardNum: CardNums | null
+}
 type CardNums = 0 | 1 | 2 | 3 | 5 | 8 | 13 | 21 | 44
-
 const Home: NextPage = () => {
+  const uri = 'http://localhost:3000'
+  const [socket] = useState(() => {
+    return io(uri)
+  })
+  const [isConnected, setIsConnected] = useState(false)
   const cardsNum: CardNums[] = [0, 1, 2, 3, 5, 8, 13, 21, 44]
+  const userId = 'test'
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('socket connected!!')
+      setIsConnected(true)
+    })
+    socket.on('disconnect', () => {
+      console.log('socket disconnected!!')
+      setIsConnected(false)
+    })
+    socket.on('room', (data: any) => {
+      console.log(data)
+    })
+
+    return () => {
+      socket.close()
+    }
+  }, [socket])
 
   const cardClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault()
     const cardNumStr = event.currentTarget.dataset.num
     if (!cardNumStr) return
     const cardNum = parseInt(cardNumStr) as CardNums
-    console.log(cardNum)
+    const data: CardType = { userId, cardNum }
+    socket.emit('room', data)
   }
 
   return (
@@ -18,6 +48,7 @@ const Home: NextPage = () => {
       <header>
         <h1 className="p-4 text-lg font-semibold">プランニングポーカー部屋</h1>
       </header>
+      <div className="p-4">{isConnected ? 'コネクト中' : 'ディスコネクト中'}</div>
       <div>
         {cardsNum.map((num, index) => {
           return <Card key={index} num={num} onClick={cardClick} />
--- server/src/events.gateway.ts
+++ server/src/events.gateway.ts
@@ -25,6 +25,7 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
   handleConnection(client: Socket, ..._args: any[]) {
     this.logger.log(`Client connected:    ${client.id}`)
   }
+
   handleDisconnect(client: Socket) {
     this.logger.log(`Client disconnected: ${client.id}`)
   }
@@ -32,6 +33,12 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
   @SubscribeMessage('message')
   handleMessage(@MessageBody() data: any): WsResponse<string> {
     this.logger.log(`Send message: ${JSON.stringify(data)}`)
-    return { event: 'message', data: data }
+    return { event: 'message', data }
+  }
+
+  @SubscribeMessage('room')
+  handleRoom(@MessageBody() data: any): WsResponse<string> {
+    this.logger.log(`${JSON.stringify(data)}`)
+    return { event: 'room', data }
   }
 }
