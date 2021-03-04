9a724eee534554e90eab1f0280c44eae61c8022d
necocoa <joyds217@gmail.com>
2021-03-05 04:21:43 +0900

    リファクタ

--- client/src/libs/common.ts
+++ client/src/libs/common.ts
@@ -1,5 +1,4 @@
 export const getUniqueStr = (myStrong?: number): string => {
-  let strong = 1000
-  if (myStrong) strong = myStrong
+  const strong = myStrong || 1000
   return new Date().getTime().toString(16) + Math.floor(strong * Math.random()).toString(16)
 }
