49b4f22be2aa88694db78ae118c7f3488745f049
necocoa <joyds217@gmail.com>
2021-03-07 01:33:39 +0900

    行並び替え

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   root: true,
+  env: { es2020: true, node: true, jest: true },
   parser: '@typescript-eslint/parser',
   parserOptions: { sourceType: 'module', project: './tsconfig.json' },
-  env: { es2020: true, jest: true, node: true },
   plugins: ['import', 'simple-import-sort'],
   extends: [
     'eslint:recommended',
@@ -10,18 +10,17 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     'no-console': ['error', { allow: ['warn', 'info', 'error'] }],
     'no-restricted-syntax': ['error', 'TSEnumDeclaration'],
     'prefer-arrow-callback': 'error',
     'arrow-body-style': ['error', 'always'],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    // インポート関連
     'import/newline-after-import': 'error',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
+  ignorePatterns: ['.eslintrc.js'],
 }
--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
+  env: { browser: true },
   parserOptions: { ecmaFeatures: { jsx: true } },
   settings: { react: { version: 'detect' } },
-  env: { browser: true },
   plugins: ['react-hooks'],
   extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended'],
   rules: {
     'func-style': ['error', 'expression'],
-
+    // Import 関係
+    'import/no-default-export': 'error',
+    '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
     // React 関係
     'no-restricted-imports': ['error', { paths: [{ name: 'react', importNames: ['default'] }] }],
     'react/prop-types': 'off',
@@ -15,11 +17,6 @@ module.exports = {
     'react/destructuring-assignment': ['error', 'never'],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-
-    // Import 関係
-    'import/no-default-export': 'error',
-    '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
-
     // Linkコンポーネントとa11yの干渉回避
     'jsx-a11y/anchor-is-valid': [
       'error',
